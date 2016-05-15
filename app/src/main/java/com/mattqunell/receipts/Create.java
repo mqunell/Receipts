65a88a33594ad9ec5e7c0e3f2e1487aafee63b5c
Matt Qunell <mqunell@gmail.com>
2016-05-14 23:23:12 -0700

    Declare EditTexts outside of button handlers so both buttons can access

--- app/src/main/java/com/mattqunell/receipts/Create.java
+++ app/src/main/java/com/mattqunell/receipts/Create.java
@@ -13,26 +13,30 @@ public class Create extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
 
-        // create_button_clear
+
+        // Declare the EditTexts for the button handlers
+        final EditText edittextUsername = (EditText) findViewById(R.id.create_edittext_username);
+        assert edittextUsername != null;
+
+        final EditText edittextPasswordInput = (EditText) findViewById(R.id.create_edittext_passwordInput);
+        assert edittextPasswordInput != null;
+
+        final EditText edittextPasswordConfirm = (EditText) findViewById(R.id.create_edittext_passwordConfirm);
+        assert edittextPasswordConfirm != null;
+
+
+        // buttonClear
         Button buttonClear = (Button) findViewById(R.id.create_button_clear);
         assert buttonClear != null;
         buttonClear.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Set the EditTexts to ""
-                EditText createEdittextUsername = (EditText) findViewById(R.id.create_edittext_username);
-                assert createEdittextUsername != null;
-                createEdittextUsername.setText("");
-
-                EditText createEdittextPasswordInput = (EditText) findViewById(R.id.create_edittext_passwordInput);
-                assert createEdittextPasswordInput != null;
-                createEdittextPasswordInput.setText("");
-
-                EditText createEdittextPasswordConfirm = (EditText) findViewById(R.id.create_edittext_passwordConfirm);
-                assert createEdittextPasswordConfirm != null;
-                createEdittextPasswordConfirm.setText("");
+                edittextUsername.setText("");
+                edittextPasswordInput.setText("");
+                edittextPasswordConfirm.setText("");
 
                 // Give focus to create_edittext_username
-                createEdittextUsername.requestFocus();
+                edittextUsername.requestFocus();
             }
         });
     }
