3bd755a6e14e5c3253e369cee7f88e01c497cafd
Matt Qunell <mqunell@gmail.com>
2016-05-14 23:44:06 -0700

    Make Create's submit button verify matching passwords and display Toast

--- app/src/main/java/com/mattqunell/receipts/Create.java
+++ app/src/main/java/com/mattqunell/receipts/Create.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Create extends AppCompatActivity {
 
@@ -23,6 +24,23 @@ public class Create extends AppCompatActivity {
 
         final EditText edittextPasswordConfirm = (EditText) findViewById(R.id.create_edittext_passwordConfirm);
         assert edittextPasswordConfirm != null;
+
+
+        // buttonSubmit
+        Button buttonSubmit = (Button) findViewById(R.id.create_button_submit);
+        assert buttonSubmit != null;
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Verify that the passwords are the same
+                if (edittextPasswordInput.getText().toString().equals(edittextPasswordConfirm.getText().toString())) {
+                    // Create the account - NOT YET IMPLEMENTED, currently only a Toast
+                    Toast.makeText(getApplicationContext(), "Passwords match", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
 
         // buttonClear
