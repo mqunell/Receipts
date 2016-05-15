e394432cb9e6ac7c2adfd61b7f2e3b40a53aa427
Matt Qunell <mqunell@gmail.com>
2016-05-14 21:46:13 -0700

    Make Create and Login activities; add Intents to Main buttons

---
+++ app/src/main/java/com/mattqunell/receipts/Create.java
@@ -0,0 +1,13 @@
+package com.mattqunell.receipts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
--- app/src/main/java/com/mattqunell/receipts/Main.java
+++ app/src/main/java/com/mattqunell/receipts/Main.java
@@ -1,7 +1,10 @@
 package com.mattqunell.receipts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class Main extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonCreate = (Button) findViewById(R.id.main_button_create);
+        if (buttonCreate != null) {
+            buttonCreate.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(Main.this, Create.class));
+                }
+            });
+        }
+
+        Button buttonLogin = (Button) findViewById(R.id.main_button_login);
+        if (buttonLogin != null) {
+            buttonLogin.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    startActivity(new Intent(Main.this, Login.class));
+                }
+            });
+        }
     }
 }
