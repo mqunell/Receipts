a80ccfceccfd0251f75e5e1570e0ec3ddb5c3e82
Matt Qunell <mqunell@gmail.com>
2016-05-14 23:04:27 -0700

    Add functionality to Create's clear button

--- app/src/main/java/com/mattqunell/receipts/Create.java
+++ app/src/main/java/com/mattqunell/receipts/Create.java
@@ -2,6 +2,9 @@ package com.mattqunell.receipts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Create extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class Create extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+
+        // create_button_clear
+        Button buttonClear = (Button) findViewById(R.id.create_button_clear);
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Set the EditTexts to ""
+                EditText createEdittextUsername = (EditText) findViewById(R.id.create_edittext_username);
+                createEdittextUsername.setText("");
+
+                EditText createEdittextPasswordInput = (EditText) findViewById(R.id.create_edittext_passwordInput);
+                createEdittextPasswordInput.setText("");
+
+                EditText createEdittextPasswordConfirm = (EditText) findViewById(R.id.create_edittext_passwordConfirm);
+                createEdittextPasswordConfirm.setText("");
+
+                // Give focus to create_edittext_username
+                createEdittextUsername.requestFocus();
+            }
+        });
     }
 }
