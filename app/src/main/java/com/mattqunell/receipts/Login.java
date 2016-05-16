f45d1ee466a8ab0393e0d0d85a70b4cc3f13bed1
Matt Qunell <mqunell@gmail.com>
2016-05-16 10:47:07 -0700

    Add Home class (from Login)

--- app/src/main/java/com/mattqunell/receipts/Login.java
+++ app/src/main/java/com/mattqunell/receipts/Login.java
@@ -1,7 +1,10 @@
 package com.mattqunell.receipts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button buttonLogin = (Button) findViewById(R.id.login_button_login);
+        assert buttonLogin != null;
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                // Until Create is functional, start Home by default
+                startActivity(new Intent(Login.this, Home.class));
+            }
+        });
     }
 }
