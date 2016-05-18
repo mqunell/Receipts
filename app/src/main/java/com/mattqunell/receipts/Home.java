a62482b85c9fccee421c99f8948e2bdcb8d94c6f
Matt Qunell <mqunell@gmail.com>
2016-05-17 17:55:02 -0700

    Begin creating basic layout for Add

--- app/src/main/java/com/mattqunell/receipts/Home.java
+++ app/src/main/java/com/mattqunell/receipts/Home.java
@@ -1,7 +1,9 @@
 package com.mattqunell.receipts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+
+    public void addReceipt(View v) {
+        //startActivity(new Intent(Home.this, Add.class));
+    }
+
+
+    public void newReceipts(View v) {
+        //startActivity(new Intent(Home.this, New.class));
+    }
+
+
+    public void oldReceipts(View v) {
+        //startActivity(new Intent(Home.this, Old.class));
     }
 }
