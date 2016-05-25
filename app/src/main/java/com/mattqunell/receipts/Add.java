69057e566cb160a6c8214e0843fa4c430c425bb1
Matt Qunell <mqunell@gmail.com>
2016-05-24 21:04:50 -0700

    Begin setup for testing storing receipt data in a file

--- app/src/main/java/com/mattqunell/receipts/Add.java
+++ app/src/main/java/com/mattqunell/receipts/Add.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 import java.util.Calendar;
@@ -15,6 +16,9 @@ public class Add extends AppCompatActivity {
     EditText edittextPlace;
     EditText edittextAmount;
     RadioGroup radiogroupLayoutRadio;
+    RadioButton radioCardOne;
+    RadioButton radioCardTwo;
+    RadioButton radioCardThree;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +29,15 @@ public class Add extends AppCompatActivity {
         edittextPlace = (EditText) findViewById(R.id.add_edittext_place);
         edittextAmount = (EditText) findViewById(R.id.add_edittext_amount);
         radiogroupLayoutRadio = (RadioGroup) findViewById(R.id.add_layout_radio);
+        radioCardOne = (RadioButton) findViewById(R.id.add_radio_cardOne);
+        radioCardTwo = (RadioButton) findViewById(R.id.add_radio_cardTwo);
+        radioCardThree = (RadioButton) findViewById(R.id.add_radio_cardThree);
     }
 
 
     // add_button_submit
     public void addButtonSubmit(View v) {
         /* TODO: Add/test these
-         * Acquire the date from the DatePicker
          * Figure out which card RadioButton was selected
          * Create a helper class to manage writing to/reading the file; pass data to it
          */
@@ -45,23 +51,36 @@ public class Add extends AppCompatActivity {
 
         String amount = edittextAmount.getText().toString();
 
+        //TODO: Test radiogroupLayoutRadio.getCheckedRadioButtonId()
         // Find out which RadioButton is selected
+        int cardNum;
+
+        if (radioCardOne.isChecked()) {
+            cardNum = 1;
+        }
+        else if (radioCardTwo.isChecked()) {
+            cardNum = 2;
+        }
+        else if (radioCardThree.isChecked()) {
+            cardNum = 3;
+        }
+        else {
+            cardNum = -1;
+        }
 
         // Create a helper class to manage the file?
         // DataManager.write(fullDate, place, amount, card);
 
+        /*
+         * TODO: Test CvsManager here first; try saving a file to Downloads or something, to be able
+         * TODO: to look at it while appending data and moving data from New->Old. If it works, then
+         * TODO: create the Java class to use as a helper
+         */
     }
 
 
     // add_button_clear
     public void addButtonClear(View v) {
-        /* TODO: Add/test these
-         * Set the DatePicker to the current date
-         * Clear the EditTexts
-         * Deselect radio button
-         * Remove focus? (DatePicker is the first thing)
-         */
-
         Calendar cal = Calendar.getInstance();
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
