df1cb65391705e5ef590a58c1151c10c88972ea7
Matt Qunell <mqunell@gmail.com>
2016-05-30 02:32:19 -0700

    Move CSV code from Add to new CsvManager helper class

--- app/src/main/java/com/mattqunell/receipts/Add.java
+++ app/src/main/java/com/mattqunell/receipts/Add.java
@@ -4,25 +4,11 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
-import java.util.Scanner;
 
 public class Add extends AppCompatActivity {
     DatePicker datepickerDate;
@@ -55,19 +41,18 @@ public class Add extends AppCompatActivity {
          * Create a helper class to manage writing to/reading the file; pass data to it
          */
 
+        // Parse the date
         int date = datepickerDate.getDayOfMonth();
         int month = datepickerDate.getMonth();
         int year = datepickerDate.getYear();
         String fullDate = date + "/" + month + "/" + year;
 
+        // Place and amount
         String place = edittextPlace.getText().toString();
-
         String amount = edittextAmount.getText().toString();
 
-        //TODO: Test radiogroupLayoutRadio.getCheckedRadioButtonId()
         // Find out which RadioButton is selected
         int cardNum;
-
         if (radioCardOne.isChecked()) {
             cardNum = 1;
         }
@@ -81,13 +66,8 @@ public class Add extends AppCompatActivity {
             cardNum = -1;
         }
 
-        // Create a helper class to manage the file?
-        // DataManager.write(fullDate, place, amount, card);
-
-
-
-        writeCsvFile();
-        printCsvFile();
+        //TESTING
+        CsvManager.writeCsvFile(fullDate, place, amount, cardNum, getFilesDir().toString());
     }
 
 
@@ -106,95 +86,7 @@ public class Add extends AppCompatActivity {
 
         radiogroupLayoutRadio.clearCheck();
 
-
-
-        File f = new File(getFilesDir() + "/receipts.csv");
-        f.delete();
-    }
-
-
-
-    public void writeCsvFile() {
-        String fileName = "receipts.csv";
-        String header = "Date,Place,Amount,Card";
-        String fileLoc = getFilesDir() + "/receipts.csv";
-
-        FileWriter fileWriter;
-
-        try {
-            // If the file already exists, append to it
-            if (new File(getFilesDir(), fileName).exists()) {
-                fileWriter = new FileWriter(fileLoc, true);
-            }
-            // If the file doesn't already exist, create it with a header
-            else {
-                fileWriter = new FileWriter(fileLoc);
-                fileWriter.append(header);
-                fileWriter.append("\n");
-            }
-
-            // Append the receipt data with comma deliminators, followed by a new line
-            fileWriter.append("test one");
-            fileWriter.append(",");
-            fileWriter.append("test two");
-            fileWriter.append(",");
-            fileWriter.append("test three");
-            fileWriter.append("\n");
-
-            // Flush and close fileWriter
-            System.out.println("writeCsvFile - CSV file created/appended successfully");
-            fileWriter.flush();
-            fileWriter.close();
-        }
-        catch (Exception e) {
-            System.out.println("writeCsvFile - Error in try/catch");
-            e.printStackTrace();
-        }
-    }
-
-    public ArrayList<String[]> readCsvFile() {
-        String fileLoc = getFilesDir() + "/receipts.csv";
-
-        BufferedReader fileReader;
-
-        // Create an ArrayList of String arrays to be created from the CSV file
-        ArrayList<String[]> receipts = new ArrayList<>();
-
-        // String to be used for each line of data in the CSV file
-        String line;
-
-        try {
-            if (new File(fileLoc).exists()) {
-                // Create the BufferedReader
-                fileReader = new BufferedReader(new FileReader(fileLoc));
-
-                // Skip the header, then read the file line by line
-                fileReader.readLine();
-                while ((line = fileReader.readLine()) != null) {
-                    String[] data = line.split(",");
-                    receipts.add(data);
-                }
-
-                // Close fileReader
-                fileReader.close();
-            } else {
-                System.out.println("readCsvFile - No file found to read");
-            }
-        }
-        catch (Exception e) {
-            System.out.println("readCsvFile - Error in try/catch");
-            e.printStackTrace();
-        }
-
-        return receipts;
-    }
-
-    public void printCsvFile() {
-        ArrayList<String[]> receipts = readCsvFile();
-
-        // Print each receipt individually
-        for (String[] s : receipts) {
-            System.out.println(Arrays.toString(s));
-        }
+        //TESTING
+        CsvManager.printCsvFile(getFilesDir().toString());
     }
 }
---
+++ app/src/main/java/com/mattqunell/receipts/CsvManager.java
@@ -0,0 +1,110 @@
+package com.mattqunell.receipts;
+
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CsvManager {
+    // CSV file header, comma deliminator, new line separator
+    private static final String HEADER = "Date,Place,Amount,Card";
+    private static final String COMMA = ",";
+    private static final String NEW_LINE = "\n";
+    private static final String FILE_NAME = "receipts.csv";
+
+
+    // Writes the given date, place, amount, and card number to the CSV file
+    public static void writeCsvFile(String date, String place, String amount, int cardNum, String dir) {
+        String fileLoc = dir + FILE_NAME;
+
+        FileWriter fileWriter;
+
+        try {
+            // If the file already exists, append to it
+            if (new File(fileLoc).exists()) {
+                fileWriter = new FileWriter(fileLoc, true);
+                System.out.println("Appending to existing");
+            }
+            // If the file doesn't already exist, create it with a header
+            else {
+                fileWriter = new FileWriter(fileLoc);
+                fileWriter.append(HEADER);
+                fileWriter.append(NEW_LINE);
+                System.out.println("Writing new file");
+            }
+
+            // Append the receipt data with comma deliminators, followed by a new line
+            fileWriter.append(date);
+            fileWriter.append(COMMA);
+            fileWriter.append(place);
+            fileWriter.append(COMMA);
+            fileWriter.append(amount);
+            fileWriter.append(COMMA);
+            fileWriter.append(Integer.toString(cardNum));
+            fileWriter.append(NEW_LINE);
+
+            // Flush and close fileWriter
+            System.out.println("writeCsvFile - CSV file created/appended successfully");
+            fileWriter.flush();
+            fileWriter.close();
+        }
+        catch (Exception e) {
+            System.out.println("writeCsvFile - Error in try/catch");
+            e.printStackTrace();
+        }
+    }
+
+
+    // Returns and ArrayList of String arrays, which are the receipts
+    public static ArrayList<String[]> readCsvFile(String dir) {
+        String fileLoc = dir + FILE_NAME;
+
+        BufferedReader fileReader;
+
+        // Create an ArrayList of String arrays to be created from the CSV file
+        ArrayList<String[]> receipts = new ArrayList<>();
+
+        // String to be used for each line of data in the CSV file
+        String line;
+
+        try {
+            if (new File(fileLoc).exists()) {
+                // Create the BufferedReader
+                fileReader = new BufferedReader(new FileReader(fileLoc));
+
+                // Skip the header, then read the file line by line
+                fileReader.readLine();
+                while ((line = fileReader.readLine()) != null) {
+                    String[] data = line.split(COMMA);
+                    receipts.add(data);
+                }
+
+                // Close fileReader
+                fileReader.close();
+            } else {
+                System.out.println("readCsvFile - No file found to read");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("readCsvFile - Error in try/catch");
+            e.printStackTrace();
+        }
+
+        return receipts;
+    }
+
+
+    // Prints the receipts from the CSV file - ONLY FOR TESTING PURPOSES
+    public static void printCsvFile(String dir) {
+        ArrayList<String[]> receipts = readCsvFile(dir);
+
+        // Print each receipt individually
+        for (String[] s : receipts) {
+            System.out.println(Arrays.toString(s));
+        }
+    }
+}
