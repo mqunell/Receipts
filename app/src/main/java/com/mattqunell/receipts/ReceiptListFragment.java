0290d5fbeaef8b04870a8bf08816d4c46eda1f6c
Matt Qunell <mqunell@gmail.com>
2017-09-09 14:16:32 -0700

    Implement ReceiptHolder and ReceiptAdapter
    Moved code that was accidentally written in ReceiptListActivity to
    ReceiptListFragment. The ViewHolder and Adapters were set up, and the
    dummy list of Receipts is displayed.

--- app/src/main/java/com/mattqunell/receipts/ReceiptListFragment.java
+++ app/src/main/java/com/mattqunell/receipts/ReceiptListFragment.java
@@ -2,11 +2,19 @@ package com.mattqunell.receipts;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class ReceiptListFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private ReceiptAdapter mAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle inState) {
@@ -14,6 +22,105 @@ public class ReceiptListFragment extends Fragment {
         // Inflate the layout file
         View view = inflater.inflate(R.layout.fragment_receipt_list, container, false);
 
+        mRecyclerView = view.findViewById(R.id.receipt_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        updateUi();
         return view;
+    }
+
+    // Helper method that creates and sets/updates the Adapter
+    private void updateUi() {
+
+        // Get the list of Receipts
+        List<Receipt> receipts = ReceiptBook.get(getActivity()).getReceipts();
+
+        // Create the adapter, or refresh the existing one
+        if (mAdapter == null) {
+            mAdapter = new ReceiptAdapter(receipts);
+            mRecyclerView.setAdapter(mAdapter);
+        }
+        else {
+            mAdapter.setReceipts(receipts);
+            mAdapter.notifyDataSetChanged();
+        }
+    }
+
+
+    /*
+     * ReceiptHolder: The ViewHolder
+     * Inflates and owns each individual layout (list_item_receipt) within the RecyclerView.
+     * The bind(Receipt) method is called each time a new Receipt should be displayed.
+     */
+    private class ReceiptHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private Receipt mReceipt;
+
+        private TextView mLocation;
+        private TextView mDate;
+        private TextView mAmount;
+        private TextView mCard;
+
+        public ReceiptHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_receipt, parent, false));
+            itemView.setOnClickListener(this);
+
+            // UI elements
+            mLocation = itemView.findViewById(R.id.receipt_location);
+            mDate = itemView.findViewById(R.id.receipt_date);
+            mAmount = itemView.findViewById(R.id.receipt_amount);
+            mCard = itemView.findViewById(R.id.receipt_card);
+        }
+
+        public void bind(Receipt receipt) {
+            mReceipt = receipt;
+
+            mLocation.setText(mReceipt.getLocation());
+            mDate.setText(mReceipt.getDate().toString());
+            mAmount.setText(mReceipt.getAmount().toString());
+            mCard.setText(mReceipt.getCard());
+        }
+
+        @Override
+        public void onClick(View view) {
+            // Start ReceiptPagerActivity
+        }
+    }
+
+
+    /*
+     * ReceiptAdapter: The Adapter
+     * Connects the ViewHolder and Receipts by knowing how Receipts and ReceiptBook are implemented.
+     * The overridden methods are all required and called by the RecyclerView itself.
+     */
+    private class ReceiptAdapter extends RecyclerView.Adapter<ReceiptHolder> {
+
+        private List<Receipt> mReceipts;
+
+        public ReceiptAdapter(List<Receipt> receipts) {
+            mReceipts = receipts;
+        }
+
+        @Override
+        public ReceiptHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+
+            return new ReceiptHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ReceiptHolder holder, int position) {
+            Receipt receipt = mReceipts.get(position);
+            holder.bind(receipt);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mReceipts.size();
+        }
+
+        public void setReceipts(List<Receipt> receipts) {
+            mReceipts = receipts;
+        }
     }
 }
