7994bbea72aef5b88a42bf6b990419066cd6e1a9
Matt <mqunell@gmail.com>
2017-09-14 22:39:46 -0700

    Create and show ReceiptListFragment's menu
    Created menu/fragment_receipt_list with a "New Receipt" button. Created
    drawable/ic_menu_add. Added the necessary methods to ReceiptListFragment
    to show the menu and button.

--- app/src/main/java/com/mattqunell/receipts/ReceiptListFragment.java
+++ app/src/main/java/com/mattqunell/receipts/ReceiptListFragment.java
@@ -7,6 +7,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.DateFormat;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -19,6 +22,12 @@ public class ReceiptListFragment extends Fragment {
     private ReceiptAdapter mAdapter;
 
     @Override
+    public void onCreate(Bundle inState) {
+        super.onCreate(inState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle inState) {
 
         // Inflate the layout file
@@ -29,6 +38,31 @@ public class ReceiptListFragment extends Fragment {
 
         updateUi();
         return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_receipt_list, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.new_receipt:
+                newReceipt();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // Creates a new Receipt
+    private void newReceipt() {
+        // todo: Make a new Receipt, add it to the ReceiptBook
+
+        // todo: Start ReceiptPagerActivity (and ReceiptFragment) at the new Receipt
     }
 
     // Helper method that creates and sets/updates the Adapter
@@ -86,7 +120,7 @@ public class ReceiptListFragment extends Fragment {
 
         @Override
         public void onClick(View view) {
-            // Start ReceiptPagerActivity
+            // todo: Start ReceiptPagerActivity
         }
     }
 
